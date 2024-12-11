628c5f0e4cde040963cc37417d120613a81ce2ff
Harinlearner <harinmurugesan@gmail.com>
2024-12-11 16:18:26 +0530

    test1

--- src/login.jsx
+++ src/login.jsx
@@ -40,7 +40,7 @@ function login() {
                             <br></br>
                             <br></br>
                             <label>{`Don't have account`}</label>
-                            <a type="button" onClick={()=>navigate("/register")}> Register</a>
+                            <a href="/register" type="button"> Register</a>
                         </form>
                     </div>
                 </center>
